06f564664accc7267433390434bb8cd58216a781
Alexander Krause <akr@informatik.uni-kiel.de>
2022-08-02 12:43:53 +0200

    different gRPC port for testing

--- src/test/java/net/explorviz/code/analysis/JavaParserServiceTest.java
+++ src/test/java/net/explorviz/code/analysis/JavaParserServiceTest.java
@@ -2,17 +2,12 @@ package net.explorviz.code.analysis;
 
 import static org.mockito.AdditionalAnswers.delegatesTo;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.lenient;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import com.github.javaparser.JavaParser;
 import com.github.javaparser.ast.CompilationUnit;
 import com.google.protobuf.Empty;
-import io.grpc.Server;
-import io.grpc.ServerCallHandler;
-import io.grpc.ServerInterceptor;
-import io.grpc.netty.NettyChannelBuilder;
 import io.grpc.netty.NettyServerBuilder;
 import io.grpc.testing.GrpcCleanupRule;
 import io.quarkus.test.junit.QuarkusTest;
@@ -25,6 +20,7 @@ import net.explorviz.code.proto.StructureCreateEvent;
 import net.explorviz.code.proto.StructureDeleteEvent;
 import net.explorviz.code.proto.StructureEventService;
 import net.explorviz.code.proto.StructureModifyEvent;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.junit.Rule;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -44,6 +40,9 @@ public class JavaParserServiceTest {
 
   @Inject
   JavaParserService parserService;
+
+  @ConfigProperty(name = "quarkus.grpc.clients.\"StructureEventService\".port")
+  int port;
 
   private final MutinyStructureEventServiceGrpc.StructureEventServiceImplBase serviceImpl =
       mock(MutinyStructureEventServiceGrpc.StructureEventServiceImplBase.class,
@@ -68,61 +67,14 @@ public class JavaParserServiceTest {
   @BeforeEach
   void setup() throws IOException {
 
-    System.out.println("sockets : ");
+    // server
+    this.grpcCleanup
+        .register(NettyServerBuilder.forAddress(new InetSocketAddress("localhost", this.port))
+            .directExecutor().addService(this.serviceImpl).build().start());
 
-    // TODO GrpcClient Channel override fpr JavaParserService
-
-    // this.channel = ManagedChannelBuilder.forAddress("localhost", 9001).usePlaintext().build();
-
-    // this.serviceImpl = mock(MutinyStructureEventServiceGrpc.StructureEventServiceImplBase.class,
-    // delegatesTo(new StructureEventService() {
-    //
-    // @Override
-    // public Uni<Empty> sendCreateEvent(final StructureCreateEvent request) {
-    // return Uni.createFrom().item(() -> Empty.newBuilder().build());
-    // }
-    //
-    // @Override
-    // public Uni<Empty> sendDeleteEvent(final StructureDeleteEvent request) {
-    // return Uni.createFrom().item(() -> Empty.newBuilder().build());
-    // }
-    //
-    // @Override
-    // public Uni<Empty> sendModifyEvent(final StructureModifyEvent request) {
-    // return Uni.createFrom().item(() -> Empty.newBuilder().build());
-    // }
-    // }));
-
-    // Create a server, add service, start, and register for automatic graceful shutdown.
-
-    // final Server server = ServerBuilder.forPort(9001).addService(serviceImpl).build().start();
-
-    final ServerInterceptor interceptor = mock(ServerInterceptor.class);
-
-    lenient().doAnswer(invocation -> {
-      final ServerCallHandler<?, ?> next = invocation.getArgument(2);
-      return next.startCall(invocation.getArgument(0), invocation.getArgument(1));
-    }).when(interceptor).interceptCall(any(), any(), any());
-
-    final Server server = NettyServerBuilder.forAddress(new InetSocketAddress("localhost", 9001))
-        .directExecutor().intercept(interceptor).addService(this.serviceImpl).build().start();
-
-    System.out.println("sockets : " + server.getListenSockets());
-
-    NettyChannelBuilder.forAddress(new InetSocketAddress("localhost", 9001)).usePlaintext().build();
-
-    // final Server server = InProcessServerBuilder.forName("localhost").directExecutor()
-    // .addService(serviceImpl).build().start();
-
-    // this.grpcCleanup.register(server);
-
-    // final ManagedChannel channel =
-    // ManagedChannelBuilder.forAddress("localhost", 9001).usePlaintext().build();
-
-    // System.out.println(server.getListenSockets());
-
-    // MutinyStructureEventServiceGrpc.newMutinyStub(server.get);
-
+    // channel
+    // this.grpcCleanup.register(NettyChannelBuilder
+    // .forAddress(new InetSocketAddress("localhost", port)).usePlaintext().build());
   }
 
   private CompilationUnit createUnit() {
