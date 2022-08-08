778da966655ea1ad5b75d65be2156b627b7e6f02
Alexander Krause <akr@informatik.uni-kiel.de>
2022-08-08 14:05:01 +0200

    testing jGit

--- src/main/java/net/explorviz/code/analysis/GitHelper.java
+++ src/main/java/net/explorviz/code/analysis/GitHelper.java
@@ -4,41 +4,45 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import javax.enterprise.context.ApplicationScoped;
+import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.lib.ObjectId;
 import org.eclipse.jgit.lib.ObjectLoader;
 import org.eclipse.jgit.lib.ObjectReader;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.revwalk.RevCommit;
-import org.eclipse.jgit.storage.file.FileRepositoryBuilder;
 import org.eclipse.jgit.treewalk.TreeWalk;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
+/**
+ * Injectable helper class for jGit concerns.
+ */
 @ApplicationScoped
 public class GitHelper {
 
-  private static final Logger LOGGER = LoggerFactory.getLogger(GitHelper.class);
+  // private static final Logger LOGGER = LoggerFactory.getLogger(GitHelper.class);
 
+  /**
+   * Tries to open the Git {@link Repository} based on a given folderPath.
+   *
+   * @param folderPath The absolute file path to the repo folder that contains the .git folder.
+   * @return repository A {@link Repository}.
+   * @throws IOException Thrown if JGit cannot open the Git repo.
+   */
   public Repository openGitRepository(final String folderPath) throws IOException {
 
     final File repoDir = new File(folderPath);
 
-    // now open the resulting repository with a FileRepositoryBuilder
-    final FileRepositoryBuilder builder = new FileRepositoryBuilder();
-    try (
-        Repository repository = builder.setGitDir(repoDir).readEnvironment().findGitDir().build()) {
-      if (LOGGER.isDebugEnabled()) {
-        LOGGER.debug("Having repository: {}", repository.getDirectory());
-      }
-
-      return repository;
-
-      // the Ref holds an ObjectId for any type of object (tree, commit, blob, tree)
-      // return repository.exactRef("refs/heads/master");
-    }
-
+    return Git.open(repoDir).getRepository();
   }
 
+  /**
+   * Returns the string content for a file path that was modified in a commit for a given repo.
+   *
+   * @param repo The {@link Repository}.
+   * @param commit The encompassing Git commit.
+   * @param path Path to the desired file.
+   * @return The stringified file content.
+   * @throws IOException Thrown if JGit cannot open the Git repo.
+   */
   public String getContent(final Repository repo, final RevCommit commit, final String path)
       throws IOException {
     try (TreeWalk treeWalk = TreeWalk.forPath(repo, path, commit.getTree())) {
