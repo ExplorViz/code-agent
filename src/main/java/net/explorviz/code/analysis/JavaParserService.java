03733dece900c11e797735af14b24a10f98dae5f
Alexander Krause-Glau <akr@informatik.uni-kiel.de>
2022-11-07 10:05:10 +0100

    Jgit

--- src/main/java/net/explorviz/code/analysis/JavaParserService.java
+++ src/main/java/net/explorviz/code/analysis/JavaParserService.java
@@ -1,34 +1,19 @@
 package net.explorviz.code.analysis;
 
-import com.github.javaparser.ParseResult;
-import com.github.javaparser.ParserConfiguration;
 import com.github.javaparser.StaticJavaParser;
 import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.body.FieldDeclaration;
+import com.github.javaparser.ast.body.MethodDeclaration;
+import com.github.javaparser.ast.visitor.GenericVisitorAdapter;
 import com.github.javaparser.ast.visitor.VoidVisitor;
-import com.github.javaparser.symbolsolver.JavaSymbolSolver;
-import com.github.javaparser.symbolsolver.resolution.typesolvers.CombinedTypeSolver;
-import com.github.javaparser.symbolsolver.resolution.typesolvers.JavaParserTypeSolver;
-import com.github.javaparser.symbolsolver.resolution.typesolvers.ReflectionTypeSolver;
-import com.github.javaparser.utils.SourceRoot;
-import com.github.javaparser.utils.SourceRoot.Callback;
-import io.quarkus.grpc.GrpcClient;
-import io.quarkus.runtime.StartupEvent;
-import io.quarkus.vertx.ConsumeEvent;
 import java.io.IOException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import javax.enterprise.context.ApplicationScoped;
-import javax.enterprise.event.Observes;
-import javax.inject.Inject;
 import net.explorviz.code.analysis.visitor.ClassNameVisitor;
-import net.explorviz.code.proto.StructureCreateEvent;
-import net.explorviz.code.proto.StructureEventService;
-import net.explorviz.code.proto.StructureModifyEvent;
+import net.explorviz.code.analysis.visitor.LocVisitor;
+import net.explorviz.code.proto.StructureFileEvent;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /**
  * Service which handles the parsing of source code via JavaParser. Processes source code directory
@@ -38,10 +23,7 @@ import org.slf4j.LoggerFactory;
 @ApplicationScoped
 public class JavaParserService {
 
-  private static final Logger LOGGER = LoggerFactory.getLogger(JavaParserService.class);
-
-  @GrpcClient("StructureEventService")
-  /* default */ StructureEventService structureEventService; // NOCS
+  // private static final Logger LOGGER = LoggerFactory.getLogger(JavaParserService.class);
 
   @ConfigProperty(name = "explorviz.landscape.token")
   /* default */ String landscapeToken; // NOCS
@@ -49,184 +31,70 @@ public class JavaParserService {
   @ConfigProperty(name = "explorviz.landscape.secret")
   /* default */ String landscapeSecret; // NOCS
 
-  private final ParserConfiguration config;
-
-  private final String folderPath;
-
   private final VoidVisitor<List<String>> classNameVisitor = new ClassNameVisitor();
-  // private final GenericVisitorAdapter<Integer, Integer> locCollector = new LocVisitor();
+  private final GenericVisitorAdapter<Integer, Void> locCollector = new LocVisitor();
   // private final VoidVisitor<List<String>> inheritanceCollector = new InheritanceVisitor();
   // private final VoidVisitor<List<String>> implementedInterfacesCollector =
   // new ImplementedInterfaceVisitor();
-  // private final VoidVisitor<List<String>> methodCollector = new MethodVisitor();
   // private final VoidVisitor<List<String>> methodCallCollector = new MethocCallVisitor();
   // private final VoidVisitor<List<String>> importVisitor = new ImportVisitor();
   // private final GenericVisitorAdapter<String, String> packageCollector = new
   // PackageNameVisitor();
 
 
-
   /**
-   * Constructor for this class. Injects {@link ConfigProperty} ${explorviz.watchservice.folder} for
-   * the initial analysis of the top level folder path to the source code.
+   * Analyzes a given stringified class, e.g., finds its fqn.
    *
-   * @param folderPath Absolute folder path used to setup the type solver and configure the initial
-   *        analysis of the source code.
+   * @param stringifiedClass The stringified class that will be analyzed by the JavaParser.
+   * @return List of StructureFileEvent. Often contains only one element, i.e., multiple elements
+   *         indicate a nested class.
+   * @throws IOException Throwed if the parsing of the stringified class fails.
    */
-  @Inject
-  public JavaParserService(
-      @ConfigProperty(name = "explorviz.watchservice.folder") final String folderPath) {
+  public List<StructureFileEvent> processStringifiedClass(final String stringifiedClass)
+      throws IOException {
+    final CompilationUnit cu = StaticJavaParser.parse(stringifiedClass);
 
-    this.folderPath = folderPath;
+    final List<StructureFileEvent> returnValues = new ArrayList<>();
 
-    final CombinedTypeSolver combinedTypeSolver = new CombinedTypeSolver(new ReflectionTypeSolver(),
-        new JavaParserTypeSolver(this.folderPath));
+    // full text
+    // loc
+    // # of methods
+    // # of variables
 
-    final JavaSymbolSolver symbolSolver = new JavaSymbolSolver(combinedTypeSolver);
-
-    this.config = new ParserConfiguration().setStoreTokens(true).setSymbolResolver(symbolSolver);
-
-    StaticJavaParser.getConfiguration().setSymbolResolver(symbolSolver);
-  }
-
-  /**
-   * This method listens to the Vert.x event "filechange" and will analyze a given absolute file
-   * path with the JavaParser.
-   *
-   * @param absoluteFilePath Absolute file path that will be analyzed by the JavaParser.
-   * @throws IOException Throwed if the parsing of the absoluteFilePath fails.
-   */
-  @ConsumeEvent("filechange")
-  public void processFile(final String absoluteFilePath) throws IOException {
-    final CompilationUnit cu = StaticJavaParser.parse(Paths.get(absoluteFilePath));
+    // # of constructors
+    // fqn of inheritances
+    // fqn of implemented interfaces
+    // fqn of imports
+    // fqn of outgoing method calls
 
     final List<String> classNames = new ArrayList<>();
-    LOGGER.debug("Class names:"); // NOCS
-
-    // print fqn
     this.classNameVisitor.visit(cu, classNames);
     for (final String className : classNames) {
-      LOGGER.debug("{}", className);
-      final StructureModifyEvent event = StructureModifyEvent.newBuilder()
-          .setFullyQualifiedOperationName(className).setLandscapeToken(this.landscapeToken)
-          .setLandscapeSecret(this.landscapeSecret).build();
-      this.structureEventService.sendModifyEvent(event).onItem()
-          .invoke(() -> LOGGER.debug("12ALEX Done")).onCancellation()
-          .invoke(() -> LOGGER.error("12ALEX Cancel")).onFailure()
-          .invoke(t -> LOGGER.debug("12ALEX Failure, {}", t)).await().indefinitely();
+
+      final int locWithoutComments = this.locCollector.visit(cu, null);
+
+      final int numberOfMethods = cu.findAll(MethodDeclaration.class).size();
+
+      final int numberOfFields = cu.findAll(FieldDeclaration.class).size();
+
+      final StructureFileEvent.Builder builder = StructureFileEvent.newBuilder();
+
+      builder.setFullyQualifiedOperationName(className);
+      builder.setLandscapeToken(this.landscapeToken);
+      builder.setLandscapeSecret(this.landscapeSecret);
+
+      builder.putArguments("content-file", stringifiedClass);
+
+      builder.putArguments("count-code-lines", String.valueOf(locWithoutComments));
+      builder.putArguments("count-methods", String.valueOf(numberOfMethods));
+      builder.putArguments("count-fields", String.valueOf(numberOfFields));
+
+      returnValues.add(builder.build());
     }
 
-  }
 
-  /**
-   * Recursive source code analysis with JavaParser of a given folder.
-   *
-   * @param absoluteFolderPath Absolute folder path that will be analyzed by the JavaParser.
-   * @throws IOException Throwed if the parsing of the absoluteFolderPath fails.
-   */
-  public void processFolder(final String absoluteFolderPath) throws IOException {
-    final Path pathToSource = Paths.get(absoluteFolderPath);
-    final SourceRoot sourceRoot = new SourceRoot(pathToSource);
+    return returnValues;
 
-    final List<String> className = new ArrayList<>();
-    // final List<String> superClassNames = new ArrayList<>();
-    // final List<String> implementedInterfacesClassNames = new ArrayList<>();
-    // final List<String> methodsOfClass = new ArrayList<>();
-    // final List<String> calledMethodsInClass = new ArrayList<>();
-    // final List<String> importNames = new ArrayList<>();
-
-    sourceRoot.parse("", this.config, new Callback() {
-
-      @Override
-      public Result process(final Path localPath, final Path absolutePath,
-          final ParseResult<CompilationUnit> result) {
-
-        if (result.isSuccessful() && result.getResult().isPresent()) {
-          final CompilationUnit cu = result.getResult().get();
-
-          LOGGER.debug("Class names:");
-
-          // print fqn
-          JavaParserService.this.classNameVisitor.visit(cu, className);
-          for (final String className : className) {
-            LOGGER.debug("{}", className);
-
-            // try {
-            // TimeUnit.SECONDS.sleep(20);
-            // } catch (final InterruptedException e) {
-            // TODO Auto-generated catch block
-            // e.printStackTrace();
-            // }
-
-            final StructureCreateEvent event =
-                StructureCreateEvent.newBuilder().setFullyQualifiedOperationName(className)
-                    .setLandscapeToken(JavaParserService.this.landscapeToken)
-                    .setLandscapeSecret(JavaParserService.this.landscapeSecret).build();
-            JavaParserService.this.structureEventService.sendCreateEvent(event).onItem()
-                .invoke(() -> LOGGER.debug("1ALEX Done")).onCancellation()
-                .invoke(() -> LOGGER.error("1ALEX Cancel")).onFailure()
-                .invoke(t -> LOGGER.debug("1ALEX Failure, {}", t)).await().indefinitely();
-
-            // MutinyStructureEventServiceGrpc.newMutinyStub(JavaParserService.this.channel)
-            // .sendCreateEvent(event).onItem().invoke(() -> LOGGER.debug("1ALEX Done"))
-            // .onCancellation().invoke(() -> LOGGER.error("1ALEX Cancel")).onFailure()
-            // .invoke(t -> LOGGER.debug("1ALEX Failure, {}", t)).await().indefinitely();
-
-            // JavaParserService.this.structureEventService.sendCreateEvent(event).subscribe();
-          }
-
-          className.clear();
-
-          /*
-           * System.out.println("Package:");
-           * System.out.println(JavaParserService.this.packageCollector.visit(cu, ""));
-           *
-           * System.out.println("LoC:");
-           * System.out.println(JavaParserService.this.locCollector.visit(cu, 0));
-           *
-           *
-           *
-           * System.out.println("Imports:"); JavaParserService.this.importVisitor.visit(cu,
-           * importNames); importNames.forEach(n -> System.out.println(n));
-           *
-           * System.out.println("Super classes:");
-           *
-           * JavaParserService.this.inheritanceCollector.visit(cu, superClassNames);
-           * superClassNames.forEach( n ->
-           * System.out.println(FqnCalculator.calculateFqnBasedOnImport(importNames, n)));
-           *
-           * System.out.println("Implemented interfaces:");
-           *
-           * JavaParserService.this.implementedInterfacesCollector.visit(cu,
-           * implementedInterfacesClassNames); implementedInterfacesClassNames.forEach( n ->
-           * System.out.println(FqnCalculator.calculateFqnBasedOnImport(importNames, n)));
-           *
-           * System.out.println("Contained Methods:");
-           *
-           * JavaParserService.this.methodCollector.visit(cu, methodsOfClass);
-           * methodsOfClass.forEach(n -> System.out.println(n));
-           *
-           * System.out.println("Called methods:");
-           *
-           * JavaParserService.this.methodCallCollector.visit(cu, calledMethodsInClass);
-           * calledMethodsInClass.forEach(n -> System.out.println(n));
-           *
-           * importNames.clear(); superClassNames.clear(); implementedInterfacesClassNames.clear();
-           * methodsOfClass.clear(); calledMethodsInClass.clear();
-           *
-           * System.out.println("");
-           */
-        }
-        return Result.DONT_SAVE;
-      }
-
-    });
-  }
-
-  /* default */ void onStart(@Observes final StartupEvent ev) throws IOException {
-    LOGGER.debug("Starting initial analysis of the source code for directory {} ...",
-        this.folderPath);
-    this.processFolder(this.folderPath);
   }
 
 }
---
+++ src/main/java/net/explorviz/code/analysis/git/GitAnalysis.java
@@ -0,0 +1,138 @@
+package net.explorviz.code.analysis.git;
+
+import io.quarkus.grpc.GrpcClient;
+import io.quarkus.runtime.StartupEvent;
+import java.io.IOException;
+import java.util.Collection;
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.event.Observes;
+import javax.inject.Inject;
+import net.explorviz.code.analysis.JavaParserService;
+import net.explorviz.code.proto.StructureEventServiceGrpc;
+import net.explorviz.code.proto.StructureFileEvent;
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.api.errors.NoHeadException;
+import org.eclipse.jgit.lib.PersonIdent;
+import org.eclipse.jgit.lib.Ref;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.revwalk.RevCommit;
+import org.eclipse.jgit.revwalk.RevSort;
+import org.eclipse.jgit.revwalk.RevTree;
+import org.eclipse.jgit.revwalk.RevWalk;
+import org.eclipse.jgit.treewalk.TreeWalk;
+import org.eclipse.jgit.treewalk.filter.PathSuffixFilter;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Entrypoint for this service. Expects a local path to a Git repository folder
+ * ("explorviz.repo.folder.path"). Sends the analysis's results to ExplorViz code service.
+ */
+@ApplicationScoped
+public class GitAnalysis {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(GitAnalysis.class);
+
+  @ConfigProperty(name = "explorviz.repo.folder.path")
+  /* package */ Optional<String> repoPath; // NOCS
+
+  @Inject
+  /* package */ GitHelper gitHelper; // NOCS
+
+  @Inject
+  /* package */ JavaParserService parserService; // NOCS
+
+  @GrpcClient("structureevent")
+  /* package */ StructureEventServiceGrpc.StructureEventServiceBlockingStub grpcClient; // NOCS
+
+  private void analyzeAndSendRepo() throws IOException, NoHeadException, GitAPIException { // NOPMD
+
+
+    if (this.repoPath.isEmpty()) {
+      return;
+    }
+
+    LOGGER.debug("Starting to analyze Git Repo... this might take a moment.");
+
+    try (Repository repository = this.gitHelper.openGitRepository(this.repoPath.get())) {
+
+      // get a list of all known heads, tags, remotes, ...
+      final Collection<Ref> allRefs = repository.getRefDatabase().getRefs();
+
+      // a RevWalk allows to walk over commits based on some filtering that is defined
+      try (RevWalk revWalk = new RevWalk(repository)) {
+
+        revWalk.sort(RevSort.COMMIT_TIME_DESC, true);
+        revWalk.sort(RevSort.REVERSE, true);
+
+        for (final Ref ref : allRefs) {
+          revWalk.markStart(revWalk.parseCommit(ref.getObjectId()));
+        }
+        if (LOGGER.isDebugEnabled()) {
+          LOGGER.debug("Walking all commits starting with {}, refs: {}", allRefs.size(), allRefs);
+        }
+        int count = 0;
+        for (final RevCommit commit : revWalk) {
+
+          final PersonIdent authorIdent = commit.getAuthorIdent();
+          final Date commitDate = authorIdent.getWhen();
+
+          if (LOGGER.isDebugEnabled()) {
+            LOGGER.debug("LogCommitDate: {}", commitDate);
+          }
+          count++;
+
+          final RevTree tree = commit.getTree();
+          // System.out.println("Having tree: " + tree);
+
+          // now use a TreeWalk to iterate over all files in the Tree recursively
+          // you can set Filters to narrow down the results if needed
+          try (final TreeWalk treeWalk = new TreeWalk(repository)) { // NOPMD
+            treeWalk.addTree(tree);
+            treeWalk.setRecursive(true);
+            treeWalk.setFilter(PathSuffixFilter.create(".java"));
+            while (treeWalk.next()) {
+              // System.out.println("found: " + treeWalk.getPathString());
+
+              final String fileContent =
+                  this.gitHelper.getContent(treeWalk.getObjectId(0), repository);
+
+              final List<StructureFileEvent> classes =
+                  this.parserService.processStringifiedClass(fileContent);
+
+              for (int i = 0; i < classes.size(); i++) {
+                final StructureFileEvent event = classes.get(i);
+                final StructureFileEvent eventWithTiming = StructureFileEvent.newBuilder(event)
+                    .setEpochMilli(authorIdent.getWhen().getTime()).build();
+                classes.set(i, eventWithTiming);
+                //grpcClient.sendStructureFileEvent(event).await().indefinitely();
+                grpcClient.sendStructureFileEvent(event);
+              }
+
+              if (LOGGER.isDebugEnabled()) {
+                LOGGER.debug("Classes names: {}", classes);
+              }
+
+
+
+            }
+          }
+        }
+        if (LOGGER.isDebugEnabled()) {
+          LOGGER.debug("Analyzed {} commits", count);
+        }
+      }
+    }
+  }
+
+
+  /* package */ void onStart(@Observes final StartupEvent ev)
+      throws IOException, NoHeadException, GitAPIException {
+    this.analyzeAndSendRepo();
+  }
+
+}
