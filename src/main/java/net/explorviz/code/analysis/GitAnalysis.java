60f76fe92ff65c8ffa93228a7b49db5001c66070
Alexander Krause <akr@informatik.uni-kiel.de>
2024-04-21 22:23:53 +0200

    remove send unrelated since otherwise we cannot find out the git commit order

--- src/main/java/net/explorviz/code/analysis/GitAnalysis.java
+++ src/main/java/net/explorviz/code/analysis/GitAnalysis.java
@@ -84,9 +84,6 @@ public class GitAnalysis { // NOPMD
   @ConfigProperty(name = "explorviz.landscape.token")
   /* default */ String landscapeTokenProperty;  // NOCS
 
-  @ConfigProperty(name = "explorviz.gitanalysis.send-unrelated-commits")
-  /* default */ boolean sendUnrelatedCommits;  // NOCS
-
   @ConfigProperty(name = "explorviz.gitanalysis.application-name")
   /* default */ String applicationNameProperty;  // NOCS
 
@@ -361,16 +358,7 @@ public class GitAnalysis { // NOPMD
     commitReportHandler.addToken(landscapeTokenProperty);
     commitReportHandler.addApplicationName(applicationNameProperty);
 
-    boolean isRelatedToDesiredAnalysisFolder =
-        !modifiedFiles.isEmpty() || !deletedFiles.isEmpty() || !addedFiles.isEmpty();
-
-    if (this.sendUnrelatedCommits) {// NOCS
-      exporter.sendCommitReport(commitReportHandler.getCommitReport());
-    } else if (isRelatedToDesiredAnalysisFolder) { // NOCS
-      exporter.sendCommitReport(commitReportHandler.getCommitReport());
-    }
-
-
+    exporter.sendCommitReport(commitReportHandler.getCommitReport());
   }
 
   private FileDataHandler fileAnalysis(final Repository repository, final FileDescriptor file,
